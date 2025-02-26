dac617544dc55babb2293e17cdf181f8ca464ec3
Astrid C. Sanchez L <astridsanchez@EdgarMSI.uacj.mx>
2025-02-26 09:50:48 -0700

    [INICIALIZA] el proyecto de XCode

---
+++ Agenda Shuffle/Agenda Shuffle/Agenda_ShuffleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Agenda_ShuffleApp.swift
+//  Agenda Shuffle
+//
+//  Created by Astrid C. Sanchez L. on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Agenda_ShuffleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PantallaAgenda()
+        }
+    }
+}
---
+++ Agenda Shuffle/Agenda Shuffle/PantallaAgenda.swift
@@ -0,0 +1,8 @@
+//
+//  PantallaAgenda.swift
+//  Agenda Shuffle
+//
+//  Created by Astrid C. Sanchez L. on 2/26/25.
+//
+
+import Foundation
