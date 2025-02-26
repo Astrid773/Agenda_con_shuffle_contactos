38e6c28ac4b75b2b8932e2e4018860f1c55d90e4
Astrid C. Sanchez L <astridsanchez@EdgarMSI.uacj.mx>
2025-02-26 10:20:00 -0700

    [AGREGAR] una pantalla de contacto_prevista

---
+++ Agenda Shuffle/Agenda Shuffle/Componentes/contacto_prevista.swift
@@ -0,0 +1,22 @@
+//
+//  contacto_prevista.swift
+//  Agenda Shuffle
+//
+//  Created by Astrid C. Sanchez L. on 2/26/25.
+//
+
+import SwiftUI
+
+struct ContactoPrevista: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "globe")
+            Text("Hellow Moon")
+            Text("My old friend")
+        }
+    }
+}
+
+#Preview {
+    ContactoPrevista()
+}
--- Agenda Shuffle/Agenda Shuffle/PantallaAgenda.swift
+++ Agenda Shuffle/Agenda Shuffle/PantallaAgenda.swift
@@ -5,4 +5,20 @@
 //  Created by Astrid C. Sanchez L. on 2/26/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct PantallaAgenda: View {
+    var body: some View {
+        List {
+            VStack {
+                ForEach(0...45, id: \.self) {_ in
+                    ContactoPrevista()
+                }
+            }
+            .background(Color.mint)
+        }
+    }
+}
+#Preview {
+    PantallaAgenda()
+}
